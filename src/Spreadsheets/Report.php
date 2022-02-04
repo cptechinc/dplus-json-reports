8914a3c0c35eb053a77319e14c4f159aa6243e35
Paul <pauldro952@outlook.com>
2022-02-04 15:22:20 -0600

    UPDATE: write json to spreadsheet cells

--- src/Spreadsheets/Report.php
+++ src/Spreadsheets/Report.php
@@ -2,8 +2,11 @@
 // PhpSpreadsheet Library
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Style as SpreadsheetStyles;
+use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
+use PhpOffice\PhpSpreadsheet\Cell\DataType;
 
 use Dplus\Reports\Json\Report\Json;
+
 
 /**
  * Spreadsheets\Report
@@ -15,6 +18,14 @@ use Dplus\Reports\Json\Report\Json;
 class Report {
 	protected $spreadsheet;
 	protected $json;
+
+	/** @var array Mapping of Fieldtype codes to Datatype codes*/
+	const FIELDTYPE_DATATYPE = [
+		'C' => DataType::TYPE_STRING,
+		'D' => DataType::TYPE_STRING,
+		'I' => DataType::TYPE_NUMERIC,
+		'N' => DataType::TYPE_NUMERIC
+	];
 
 	/** @var array Column Heading Styles */
 	const STYLES_COLUMN_HEADER = [
@@ -29,13 +40,35 @@ class Report {
 		],
 	];
 
+	/** @var array Record Heading Styles */
+	const STYLES_RECORD_HEADING = [
+		'font' => [
+			'bold' => true,
+			'size' => 12
+		],
+		'borders' => [
+			'top' => [
+				'borderStyle' => SpreadsheetStyles\Border::BORDER_THIN,
+			],
+		],
+		'fill' => [
+			'fillType' => SpreadsheetStyles\Fill::FILL_SOLID,
+			'startColor' => [
+				'rgb' => 'E6E6EA',
+			],
+			'endColor' => [
+				'rgb' => 'E6E6EA',
+			],
+		],
+	];
+
 	public function __construct() {
 		$this->spreadsheet = new Spreadsheet();
 	}
 
 	/**
 	 * Set Json
-	 * @param Json $json  JSON Container
+	 * @param  Json $json  JSON Container
 	 * @return void
 	 */
 	public function setJson(Json $json) {
@@ -52,26 +85,73 @@ class Report {
 	}
 
 	/**
+	 * Return Spreadsheet
+	 * @return Spreadsheet
+	 */
+	public function getSpreadsheet() {
+		return $this->spreadsheet;
+	}
+
+	/**
 	 * Populates Column Headers in the Spreadsheet
 	 * @return void
 	 */
-	private function generateHeader(){
+	protected function generateHeader() {
 		$sheet = $this->spreadsheet->getActiveSheet();
-		$colCount = count($this->json->fields);
+		$colCount = count($this->json->getFields());
 		Writer::setColumnsAutowidth($sheet, $colCount);
 
 		$row = 1;
-		$i = 1;
-		foreach ($this->json->fields as $key => $field) {
+		$i   = 1;
+		
+		foreach ($this->json->getFields() as $key => $field) {
 			$cell = $sheet->getCellByColumnAndRow($i, $row);
 			$cell->getStyle()->applyFromArray(static::STYLES_COLUMN_HEADER);
-			$cell->getStyle()->getAlignment()->setHorizontal(Writer::getAlignmentCode($field['justify']));
+			$cell->getStyle()->getAlignment()->setHorizontal(Writer::getAlignmentCode($this->json->getFieldJustify($key)));
 			$cell->setValue($field['label']);
+
 			$i++;
 		}
 	}
 
-	private function generateBody() {
+	protected function generateBody() {
+		$sheet = $this->spreadsheet->getActiveSheet();
+		$row   = $this->getNewRowIndex();
+		$data  = $this->json->getData();
+		$colCount = count($this->json->getFields());
 
+		foreach ($data as $record) {
+			$cell = $sheet->getCellByColumnAndRow(1, $row);
+			$cell->getStyle()->applyFromArray(static::STYLES_RECORD_HEADING);
+			$cell->setValue($record['header']);
+
+			
+			$c1 = Coordinate::stringFromColumnIndex(1) . $row;
+			$c2 = Coordinate::stringFromColumnIndex($colCount + 1) . $row;
+			$sheet->mergeCells("$c1:$c2");
+			$row++;
+
+			foreach ($record['detail'] as $detail) {
+				$i = 1;
+
+				foreach ($this->json->getFields() as $key => $field) {
+					$cell = $sheet->getCellByColumnAndRow($i, $row);
+					$cell->getStyle()->getAlignment()->setHorizontal(Writer::getAlignmentCode($this->json->getFieldJustify($key)));
+					$cell->setValueExplicit($detail[$key], static::FIELDTYPE_DATATYPE[$field['type']]);
+					$i++;
+				}
+				$row++;
+			}
+		}
+	}
+
+	/**
+	 * Return Index for the next available blank row
+	 * @param  Worksheet $sheet
+	 * @return int
+	 */
+	protected function getNewRowIndex(Worksheet $sheet = null) {
+		$sheet = $sheet ? $sheet : $this->spreadsheet->getActiveSheet();
+		return $sheet->getHighestRow() + 1;
 	}
 }
